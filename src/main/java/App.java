c2489f2dff147a2d769edc1207df1f9578778687
Artem Pronkin <artempro1994@gmail.com>
2023-10-18 16:55:00 +0500

    1st commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat)applicationContext.getBean("cat");
+        Cat cat1 = (Cat)applicationContext.getBean("cat");
+
+        System.out.println(bean.equals(bean1));
+        System.out.println(cat1.equals(cat));
         System.out.println(bean.getMessage());
     }
 }
